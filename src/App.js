86a5bc64ae3c285a7c2517bb60a3f7fcb8aebfe5
BaGorK <edmealemkassahun@gmail.com>
2024-02-14 21:29:32 +0300

    Building the project UI with static data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+const questions = [
+  {
+    question: 'Which is the most popular JavaScript framework?',
+    options: ['Angular', 'React', 'Svelte', 'Vue'],
+    correctOption: 1,
+    points: 10,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='header'>
+        <div className='img-container'>
+          <img src='logo192.png' alt='logo' />
+        </div>
+        <h1>The React Quiz</h1>
       </header>
+
+      <main className='main'>
+        <div className='question-container'>
+          <p>Which is the most popular JavaScript framework?</p>
+          <div>
+            <span role='button' className='choice'>
+              State
+            </span>
+            <span role='button' className='choice'>
+              Props
+            </span>
+            <span role='button' className='choice'>
+              PropTypes
+            </span>
+            <span role='button' className='choice'>
+              Parameters
+            </span>
+          </div>
+        </div>
+        <div className='btn-container'>
+          <button className='btn btn-timer'>06:17</button>
+          <button className='btn btn-next'>Next</button>
+        </div>
+      </main>
     </div>
   );
 }
